7974f495d5c5b4375bb9ab19d67fb92fdb1019f3
José <jfmdev@gmail.com>
2021-01-14 13:44:23 -0300

    Fix hybrid agent and improve linear predictor agent on Rock Paper Scissors bot

--- rock-paper-scissors/agents.py
+++ rock-paper-scissors/agents.py
@@ -35,6 +35,14 @@ class Observation:
     self.step = step
     self.lastOpponentAction = action
 
+class Round:
+    def __init__(self, action_1, action_2):
+        self.action_1 = action_1
+        self.action_2 = action_2
+        
+    def __repr__(self):
+        return "Round({0}|{1})".format(self.action_1, self.action_2)
+
 
 # --- Stateless agents --- #
 
@@ -116,34 +124,61 @@ class StatisticalAgent:
         else:
             return random_agent(observation, configuration)
 
-# TODO: Allow to define 'depth'.
+# Assumes player will repeat move if previous rounds are the same.
 class LinearPredictorAgent:
-    def __init__(self):
-        self.rival_penultimate = None
-        self.my_penultimate = None
+    def __init__(self, depth=2):
+        self.depth = depth
         self.my_last = None
-        self.predictions = [None]*9
-
+        self.previous_rounds = []
+        
+        self.prediction_maps = [None]*depth
+        for i in range(depth):
+            map_size = 9**(i+1)
+            self.prediction_maps[i] = [None]*map_size
+         
+    def get_pred_index(self, map_index):
+        if map_index < len(self.previous_rounds):
+            pred_index = 0
+            
+            for k in range(map_index+1):
+                round_k = self.previous_rounds[k]
+                pred_index += (round_k.action_1*3 + round_k.action_2) * (9**k)
+                
+            return pred_index
+        
+        return None
+         
     def play(self, observation, configuration):
-        # Update prediction map.
-        if observation.step > 1:
-            pred_index = self.my_penultimate*3 + self.rival_penultimate
-            self.predictions[pred_index] = observation.lastOpponentAction
-
-        # Update flags and predict next move.
         if observation.step > 0:
-            self.rival_penultimate = observation.lastOpponentAction
-            self.my_penultimate = self.my_last
-    
-            pred_index = self.my_last * 3 + observation.lastOpponentAction
-            if self.predictions[pred_index] != None:
-                self.my_last = winning_action(self.predictions[pred_index])
-            else:
-                self.my_last = None
+            # Update prediction map.
+            for i in range(self.depth):
+                pred_map = self.prediction_maps[i]
+                pred_index = self.get_pred_index(i)
+                
+                if pred_index is not None:
+                    pred_map[pred_index] = observation.lastOpponentAction
 
+            # Update history.
+            last_round = Round(self.my_last, observation.lastOpponentAction)
+            self.previous_rounds.insert(0, last_round)
+            if len(self.previous_rounds) > (self.depth + 1):
+                # Remove entries we don't longer need.
+                self.previous_rounds.pop()
+            
+            # Get best prediction.
+            self.my_last = None
+            for p in reversed(range(self.depth)):
+                pred_map = self.prediction_maps[p]
+                pred_index = self.get_pred_index(p)
+                
+                prediction = pred_map[pred_index] if pred_index is not None else None
+                if prediction is not None:
+                    self.my_last = winning_action(prediction)
+                    break
+                
         # If can't predict, use the good old random (nothing beats that).
         if self.my_last == None:
-            self.my_last = random_agent(observation, configuration)
+            self.my_last = 0 #random_agent(observation, configuration)
             
         return self.my_last
 
@@ -160,7 +195,10 @@ class HybridAgent:
             reactionary_agent,
             BeatHumanAgent(),
             StatisticalAgent(),
-            LinearPredictorAgent()
+            LinearPredictorAgent(1),
+            LinearPredictorAgent(2),
+            LinearPredictorAgent(3),
+            LinearPredictorAgent(4)
         ]
         self.scores = [0]*len(self.agents)
         self.last_moves = [0]*len(self.agents)
@@ -187,6 +225,12 @@ class HybridAgent:
             # The first time just use the random agent.
             next_move = self.last_moves[0]
         
+        # HACK: Update last action on agents (otherwise their records won't match).
+        for i in range(len(self.agents)):
+            agent = self.agents[i]
+            if hasattr(agent, 'my_last'):
+                agent.my_last = next_move
+        
         # Play.
         return next_move
 
@@ -197,10 +241,9 @@ def final_player(observation, configuration):
     return my_agent.play(observation, configuration)
 
 # --- Local testing --- #
-
 '''
 # Configuration variables.
-rounds = 40
+rounds = 512
 player_1 = reactionary_agent
 player_2 = HybridAgent()
 
